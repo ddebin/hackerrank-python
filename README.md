13aaaccfec9f8600dc29ed9f03f39d27c1cff1f4
Damien Debin <damien.debin@gmail.com>
2025-09-30 18:17:55 +0200

    feat: add climbing-the-leaderboard

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 - [`the-grid-search`](src/the_grid_search.py): [The Grid Search](https://www.hackerrank.com/challenges/the-grid-search/)
 - [`bigger-is-greater`](src/bigger_is_greater.py): [Bigger is Greater](https://www.hackerrank.com/challenges/bigger-is-greater/)
+- [`climbing-the-leaderboard`](src/climbing_the_leaderboard.py): [Climbing the Leaderboard](https://www.hackerrank.com/challenges/climbing-the-leaderboard/)
 
 ### Hard difficulty
 
---
+++ src/climbing_the_leaderboard.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from typing import IO
+
+
+def climbingLeaderboard(ranked: list[int], player: list[int]) -> list[int]:
+    ranks = []
+
+    current_rank = 1
+    last_ranked_score = -1
+    for ranked_score in ranked:
+        while len(player) > 0 and player[-1] >= ranked_score:
+            ranks.insert(0, current_rank)
+            player.pop()
+
+        if ranked_score != last_ranked_score:
+            current_rank += 1
+            last_ranked_score = ranked_score
+
+    while len(player) > 0:
+        ranks.insert(0, current_rank)
+        player.pop()
+
+    return ranks
+
+
+def main(fptr: IO) -> None:
+    ranked_count = int(input().strip())
+    ranked = list(map(int, input().rstrip().split()))
+    assert len(ranked) == ranked_count
+    player_count = int(input().strip())
+    player = list(map(int, input().rstrip().split()))
+    assert len(player) == player_count
+    result = climbingLeaderboard(ranked, player)
+    fptr.write("\n".join(map(str, result)) + "\n")
+
+
+if __name__ == "__main__":
+    if "OUTPUT_PATH" in os.environ:
+        fptr = open(os.environ["OUTPUT_PATH"], "wt")
+    else:
+        fptr = sys.stdout
+
+    main(fptr)
+
+    fptr.close()

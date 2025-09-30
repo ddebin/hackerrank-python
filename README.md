599f5fceb4f5a33608167a6f166397413e9d8e70
Damien Debin <damien.debin@gmail.com>
2025-09-30 17:50:11 +0200

    feat: add morgan-and-a-string

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 - [`the-grid-search`](src/the_grid_search.py): [The Grid Search](https://www.hackerrank.com/challenges/the-grid-search/)
 - [`bigger-is-greater`](src/bigger_is_greater.py): [Bigger is Greater](https://www.hackerrank.com/challenges/bigger-is-greater/)
+
+### Expert difficulty
+
+- [`morgan-and-a-string`](src/morgan_and_a_string.py): [Morgan and a String](https://www.hackerrank.com/challenges/morgan-and-a-string/)
---
+++ src/morgan_and_a_string.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from typing import IO
+
+
+def morganAndString(fptr, a: str, b: str) -> None:
+    i = j = 0
+    # '~' is > 'z'
+    while not a[i] == b[j] == "~":
+        if a[i:] < b[j:]:
+            fptr.write(a[i])
+            i += 1
+        else:
+            fptr.write(b[j])
+            j += 1
+
+
+def main(fptr: IO) -> None:
+    t = int(input().strip())
+    for _itr in range(t):
+        a = input() + "~"
+        b = input() + "~"
+        morganAndString(fptr, a, b)
+        fptr.write("\n")
+
+
+if __name__ == "__main__":
+    if "OUTPUT_PATH" in os.environ:
+        fptr = open(os.environ["OUTPUT_PATH"], "wt")
+    else:
+        fptr = sys.stdout
+
+    main(fptr)
+
+    fptr.close()
